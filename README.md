d3c82f2c98f1e038d0bcf7f7f1eb8e8d835a6f2f
Fisher <truefisherking@gmail.com>
2017-11-11 18:52:44 -0500

    Followed Steps from Travery Media

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Learning Node Tutorial
 
+first steps to understanding Node.js:
+[Node.js Tutorial For Absolut Beginners](https://www.youtube.com/watch?v=U8XF6AFGqlc)
+by
+[Traversy Media](https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA)
+
 walking myself through Node.js
 ## [Learning Node.js](https://www.lynda.com/Node-js-tutorials/Learning-Node-js/612195-2.html)
---
+++ myfirst.js
@@ -0,0 +1,14 @@
+// var http = require('http');
+//
+// http.createServer(function (req, res){
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('Hello World!');
+//   console.log('This example is different!');
+// console.log('The result is displayed in the Command Line Interface');
+// }).listen(8080);
+
+
+// function greeting(greet){
+//   console.log(greet);
+// }
+// greeting('Hello World!');
